16df68d5640d781f0760b9463fbb61d7ecd6ceb2
Ahsaan Ansari <ahsaan.se15@gmail.com>
2020-03-06 13:41:22 +0500

    Add project files.

---
+++ DataGridSample/DataGridSample/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DataGridSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class ImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            if (value != null)
+            {
+                 return ImageSource.FromResource("DataGridSample.dna.png");
+            }
+            else
+                return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataGridSample/DataGridSample/ViewModel/ViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Syncfusion.Data.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridSample
+{
+    public class ViewModel : NotificationObject
+    {
+        public ObservableCollection<string> CustomerNames { get; set; }
+        public string[] Customers = new string[] { "Adams", "Tim", "Rooney", "Andrew", "Harry" };
+
+        OrderInfoRepository order;
+        public ViewModel()
+        {
+            this.CustomerNames = Customers.ToObservableCollection();
+            SetRowstoGenerate(20);
+        }
+
+        #region ItemsSource
+
+        private ObservableCollection<OrderInfo> ordersInfo;
+        public ObservableCollection<OrderInfo> OrdersInfo
+        {
+            get { return ordersInfo; }
+            set { this.ordersInfo = value; }
+        }
+
+        #endregion
+
+        #region ItemSource Generator
+
+        public void SetRowstoGenerate(int count)
+        {
+            order = new OrderInfoRepository();
+            ordersInfo = order.GetOrderDetails(count);
+        }
+
+        #endregion
+    }
+
+    public class NotificationObject : INotifyPropertyChanged
+    {
+        public void RaisePropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
